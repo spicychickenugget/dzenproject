9dccdfd59333fe139bda1a4878af99d963f11972
spicychickenugget <chefrouxflorida@gmail.com>
2025-04-12 20:52:11 -0400

    Update Chart.js content

--- src/Chart.js
+++ src/Chart.js
@@ -1,5 +1,6 @@
 import { Pie } from 'react-chartjs-2';
 import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
+import React from 'react';
 
 ChartJS.register(ArcElement, Tooltip, Legend);
 
@@ -14,3 +15,34 @@ const TokenomicsChart = ({ tokenData }) => {
       },
     ],
   };
+
+  const options = {
+    responsive: true,
+    maintainAspectRatio: false, // Allows custom height
+    plugins: {
+      legend: {
+        position: 'bottom', // Better for mobile
+        labels: {
+          font: {
+            size: 14,
+          },
+        },
+      },
+    },
+  };
+
+  return (
+    <div
+      style={{
+        width: '100%',
+        maxWidth: '400px',
+        height: '300px',
+        margin: '0 auto',
+      }}
+    >
+      <Pie data={data} options={options} />
+    </div>
+  );
+};
+
+export default TokenomicsChart;
